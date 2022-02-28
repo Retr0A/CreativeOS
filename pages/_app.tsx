33d0c0a3df5b80e2a05afdd7bf91baf18a6bd365
Retr0A <92212390+Retr0A@users.noreply.github.com>
2022-02-28 14:48:21 +0200

    Code Checking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import 'styles/globals.scss';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { description, name } from 'package.json';
 import type { ReactElement } from 'react';
 
-export default function MyApp({
-  Component,
-  pageProps,
-}: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <>
+      <Head>
+        <title>{ name }</title>
+        <meta name="description" content={ description } />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-duplicates */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import type { ReactElement } from 'react';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
